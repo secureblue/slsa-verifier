b4b9b473465be2c855ee4083c159022ce2c3841f
Daniel Hast <hast.daniel@protonmail.com>
2026-06-07 18:50:00 -0400

    fix(build): make %autochangelog work, use %autorelease
    Manually apply `rpmautospec` to make `%autochangelog` and `%autorelease`
    work in Copr.

--- copr_script.sh
+++ copr_script.sh
@@ -1,26 +1,25 @@
-#! /bin/bash -x
+#!/bin/sh
 
-# Copyright 2025 The Secureblue Authors
+# SPDX-FileCopyrightText: Copyright 2025-2026 The Secureblue Authors
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software distributed under the License is
-# distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and limitations under the License.
+# SPDX-License-Identifier: Apache-2.0
 
-set -oue pipefail
+# Package dependencies for this script:
+# git-core go2rpm go-vendor-tools python3-specfile rpmautospec rpmdevtools
 
-cd slsa-verifier
-VERSION=$(grep -E '^Version:' slsa-verifier.spec | awk '{print $2}')
+set -eux
+
+VERSION=$(awk '/^Version:[[:blank:]]/ { print $2; exit }' slsa-verifier/slsa-verifier.spec)
+(
+    cd slsa-verifier
+    cp go-vendor-tools.toml ..
+    rpmautospec process-distgit ./slsa-verifier.spec ../slsa-verifier.spec
+)
 mkdir -p generate_vendor
 cp go-vendor-tools.toml generate_vendor
-cd generate_vendor
-go2rpm --name slsa-verifier --profile vendor --version "${VERSION}" -s cli/slsa-verifier https://github.com/slsa-framework/slsa-verifier
-mv "slsa-verifier-${VERSION}-vendor.tar.bz2" ..
-cd ..
-rm generate_vendor -rf
-mv ./* ../
+(
+    cd generate_vendor
+    go2rpm --name slsa-verifier --profile vendor --version "${VERSION}" -s cli/slsa-verifier https://github.com/slsa-framework/slsa-verifier
+    mv "slsa-verifier-${VERSION}.tar.gz" "slsa-verifier-${VERSION}-vendor.tar.bz2" ..
+)
+rm -rf slsa-verifier generate_vendor
